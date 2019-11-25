d0a113b8d7dfd7520632dc3f93e1429d3854a93f
iwansafr <iwansafr@gmail.com>
2019-11-25 10:33:27 +0700

    trash model

---
+++ application/modules/admin/controllers/Shop.php
@@ -0,0 +1,33 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Shop extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->db->cache_off();
+		$this->load->model('esg_model');
+		$this->load->model('admin_model');
+		$this->load->model('admin_menu_model');
+		$this->load->model('shop_model');
+		$this->load->library('esg');
+		$this->load->library('ZEA/zea');
+		$this->esg_model->init();
+	}
+
+	public function index()
+	{
+		$this->load->view('index');
+	}
+
+	public function restore($id = 0)
+	{
+		$this->load->view('index',['data'=>$this->trash_model->restore($id)]);
+	}
+
+	public function detail($id = 0)
+	{
+		$this->load->view('index',['data'=>$this->trash_model->detail($id)]);
+	}
+
+}
